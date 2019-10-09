87d1b65afd6b92ba5bb822b3ade9e89c562caaa9
Ben Grabowski <gbowski30@gmail.com>
2019-10-09 08:34:12 -0600

    added notesRouter and foldersRouter

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -8,7 +8,7 @@ const jsonParser = express.json()
 
 const sanitizeFolder = folder => ({
     id: folder.id,
-    name: xss(folder.folder_name)
+    folder_name: xss(folder.folder_name)
 })
 
 foldersRouter
@@ -22,6 +22,9 @@ foldersRouter
         })
         .catch(next)
     })
+
+foldersRouter
+    .route('/folders/addFolder')
     .post(jsonParser, (req, res, next) => {
         const { folder_name } = req.body
         const newFolder = {folder_name}
@@ -47,10 +50,58 @@ foldersRouter
 
 
 foldersRouter
-    .route('/:folder_id')
-    .all()
-    .get()
-    .delete()
-    .patch()
+    .route('/folders/:folder_id')
+    .all((req, res, next) => {
+        FoldersService.getById(
+            req.app.get('db'),
+            req.params.folder_id
+        )
+            .then(folder => {
+                if (!folder) {
+                    return res.status(404).json({
+                        error: { message: `Folder doesn't exist` }
+                    })
+                }
+                res.folder = folder
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(sanitizeFolder(res.folder))
+    })
+    .delete((req, res, next) => {
+        FoldersService.deleteFolder(
+            req.app.get('db'),
+            req.params.folder_id
+        )
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const { folder_name } = req.body
+        const folderToUpdate = folder_name
+
+        const numberOfValues = Object.values(folderToUpdate).filter(Boolean).length
+        if (numberOfValues === 0) {
+            return res.status(400).json({
+                error: {
+                    message: `Request body must contain a folder name`
+                }
+            })
+        }
+
+        FoldersService.updateFolder(
+            req.app.get('db'),
+            req.params.folder_id,
+            folderToUpdate
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
 
     module.exports = foldersRouter;
---
+++ src/notes/notes-router.js
@@ -0,0 +1,111 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const NotesService = require('./notes-service')
+
+const notesRouter = express.Router()
+const jsonParser = express.json() 
+
+const sanitizeNote = note => ({
+    id: note.id,
+    note_name: xss(note.note_name),
+    content: xss(note.content),
+    modified: note.modified,
+    folder: note.folder
+})
+
+notesRouter
+    .route('/')
+    .get((req, res, next) => {
+        notesService.getAllnotes(
+            req.app.get('db')
+        )
+        .then(notes => {
+            res.json(notes.map(sanitizeNote))
+        })
+        .catch(next)
+    })
+
+notesRouter
+    .route('/notes/addNote')
+    .post(jsonParser, (req, res, next) => {
+        const { note_name, content, modified, folder } = req.body
+        const newNote = { note_name, modified, folder }
+
+        for (const [key, value] of Object.entries(newNote)) {
+            if (value == null) {
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` }
+                })
+            }
+        }
+        newNote.content = content
+        NotesService.insertNote(
+            req.app.get('db'),
+            newNote
+        )
+            .then(note => {
+                res
+                    .status(204)
+                    .location(path.posix.join(req.originalUrl, `/${note.id}`))
+                    .json(sanitizeNote(note))
+            })
+            .catch(next)
+    })
+
+notesRouter
+    .route('/notes/:note_id')
+    .all((req, res, next) => {
+        NotesService.getById(
+            req.app.get('db'),
+            req.params.note_id
+        )
+            .then(note => {
+                if (!note) {
+                    return res.status(404).json({
+                        error: { message: `Note doesn't exist` }
+                    })
+                }
+                res.note = note
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(sanitizeNote(res.note))
+    })
+    .delete((req, res, next) => {
+        NotesService.deleteNote(
+            req.app.get('db'),
+            req.params.note_id
+        )
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const { note_name, content, modified, folder } = req.body
+        const noteToUpdate = { note_name, content, modified, folder }
+
+        const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length
+        if (numberOfValues === 0) {
+            return res.status(400).json({
+                error: {
+                    message: `Request body must contain either 'Note Name', 'Content', 'Modified Date', or 'Folder'`
+                }
+            })
+        }
+
+        NotesService.updateNote(
+            req.app.get('db'),
+            req.paramas.note_id,
+            noteToUpdate
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+    module.exports = notesRouter
