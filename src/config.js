9d6b950ca121a8d5c0b83df0f10c035f594a7a8c
Ben Grabowski <gbowski30@gmail.com>
2019-10-11 20:56:37 -0600

    changed DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
+    DATABASE_URL: 'postgres://vbmhhsduahsnng:f0f68dd5df0be93ac7ccf22a02e3e580208c08d324cb9df7f463261fd723b13c@ec2-174-129-238-192.compute-1.amazonaws.com:5432/d45i7e34d9ene9',
 }
